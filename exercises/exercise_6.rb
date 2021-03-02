2239c335c4ed7e9e7ed23a09a3af182f09d8cc4b
Louis Coreas <l.g.coreas@gmail.com>
2021-03-02 21:02:31 +0000

    exercise 4,5,6.7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Louis", last_name: "Coreas", hourly_rate: 50)
+@store1.employees.create(first_name: "Apurva", last_name: "Jani", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Shay", last_name: "Climaco", hourly_rate: 40)
+@store2.employees.create(first_name: "Jason", last_name: "Bourne", hourly_rate: 40)
+@store2.employees.create(first_name: "Scott", last_name: "Pilgrim", hourly_rate: 42)
+
+
+puts "Store 1 has #{@store1.employees.count} employees"
+puts "Store 2 has #{@store2.employees.count} employees"
+puts "Total number of employees at all stores: #{Employee.count}"
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "What's your store's name? "
+store_name = gets.chomp
+
+@new_store = Store.create(name: store_name, annual_revenue: "", mens_apparel:"" , womens_apparel:"")
+
+# if @new_store.errors.any?
+#   @new_store.errors.each do |attribute, message|
+#     puts "Error for #{attribute}: #{message}"
+#   end
+# end
